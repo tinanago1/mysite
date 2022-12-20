c9b0a4d205f23b91303bb6c2e29436b72afcf646
tinanago1 <sr9plite@gmail.com>
2022-12-20 22:41:57 +0900

    プロジェクト整理とdartsass開発環境構築

--- _gulp/gulpfile.js
+++ _gulp/gulpfile.js
@@ -1,94 +1,21 @@
-const gulp = require('gulp');//gulp本体
+// 1.モジュール読み込み
+//----------------------------------------------------------------------
+// gulpの読み込み
+const gulp = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 
-//scss
-const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
-const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
-const notify = require("gulp-notify"); // エラー発生時のアラート出力
-const browserSync = require("browser-sync"); //ブラウザリロード
+// sassの読み込み
+const gulpDartSass = require('gulp-dart-sass');
 
-
-// 入出力するフォルダを指定
-const srcBase = '../_static/src';
-const assetsBase = '../_assets';
-const distBase = '../_static/dist';
-
-
-const srcPath = {
-  'scss': assetsBase + '/scss/**/*.scss',
-  'html': srcBase + '/**/*.html'
-};
-
-const distPath = {
-  'css': distBase + '/css/',
-  'html': distBase + '/'
-};
-
-/**
- * sass
- *
- */
-const cssSass = () => {
-  return gulp.src(srcPath.scss, {
-    sourcemaps: true
-  })
-    .pipe(
-      //エラーが出ても処理を止めない
-      plumber({
-        errorHandler: notify.onError('Error:<%= error.message %>')
-      }))
-    .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
-    .pipe(gulp.dest(distPath.css, { sourcemaps: './' })) //コンパイル先
-    .pipe(browserSync.stream())
-    .pipe(notify({
-      message: 'Sassをコンパイルしました！',
-      onLast: true
-    }))
+// 2.タスク定義
+//----------------------------------------------------------------------
+const sass = () => {
+  return gulp
+    .src('../_src/Layout/*.scss')
+    .pipe(gulpDartSass())
+    .pipe(gulp.dest('dist/css'))
 }
 
-
-/**
- * html
- */
-const html = () => {
-  return gulp.src(srcPath.html)
-    .pipe(gulp.dest(distPath.html))
-}
-
-/**
- * ローカルサーバー立ち上げ
- */
-const browserSyncFunc = () => {
-  browserSync.init(browserSyncOption);
-}
-
-const browserSyncOption = {
-  server: distBase
-}
-
-/**
- * リロード
- */
-const browserSyncReload = (done) => {
-  browserSync.reload();
-  done();
-}
-
-/**
- *
- * ファイル監視 ファイルの変更を検知したら、browserSyncReloadでreloadメソッドを呼び出す
- * series 順番に実行
- * watch('監視するファイル',処理)
- */
-const watchFiles = () => {
-  gulp.watch(srcPath.scss, gulp.series(cssSass))
-  gulp.watch(srcPath.html, gulp.series(html, browserSyncReload))
-}
-
-/**
- * seriesは「順番」に実行
- * parallelは並列で実行
- */
-exports.default = gulp.series(
-  gulp.parallel(html, cssSass),
-  gulp.parallel(watchFiles, browserSyncFunc)
-);
+// 3.実行
+//----------------------------------------------------------------------
+exports.sass = sass
