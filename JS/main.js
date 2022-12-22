d768a24b29cd6f24326a5c153db40eb75005b6c2
tinanago1 <sr9plite@gmail.com>
2022-12-23 04:34:30 +0900

    追記

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,13 @@
-const search = document.querySelector(".L-header_formWrap_form_search");
 
-function inputclick () {
-    alert("click");
-}
 
-search.addEventListener("click", inputclick);
+document.querySelector(".L-header_formWrap_form_search").addEventListener("click", () => {
+
+    const element = document.querySelector(".L-header_formWrap_searchIcon_img");
+    element.classList.add("addClass");
+    
+});
+
+
+
+
+
