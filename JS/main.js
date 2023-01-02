b9b3eb74e50ccc60a216fb8c38aba5a85d7e49ab
tinanago1 <sr9plite@gmail.com>
2023-01-03 02:15:11 +0900

    追記

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,10 @@
 // HTML elemental <====
 const inputSearch = document.querySelector(".L-header_formWrap_form_search");
 const searchIcon = document.querySelector(".L-header_formWrap_searchIcon_img");
-const menuButton = document.querySelector(".mobileMenuContainer");
-const container = document.querySelector("#globalContainer");
+const menuButton = document.querySelector(".L-header_mobileMenuContainer");
+const mobileMenu = document.querySelector(".mobileMenu");
+const body = document.querySelector("body");
+const container = document.querySelector(".container");
 
 // HTML elemental ===>
 
@@ -23,12 +25,21 @@ const clear = () => {
 clear();
 // inputSearch ===>
 
+// mobileMenu ==>
 const menuSwitch = () => {
     menuButton.addEventListener("click", () => {
-        container.classList.toggle("menu-on-of");
-        
+        mobileMenu.classList.toggle("mobileMenuOn");
+
+        if(mobileMenu.classList.contains("mobileMenuOn")) {
+            body.style.cssText = "overflow: hidden;" 
+            // container.style.cssText = "pointer-events: none;"
+        } else {
+            body.style.cssText = "overflow: scroll;"
+        //     container.style.cssText = "pointer-events: initial;"
+        }
     })
 }
+// mobileMenu ==>
 
 menuSwitch();
 
