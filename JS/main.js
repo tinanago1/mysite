b1128a7693d60bacc1a1ba1a3bed2c0eec54bc83
tinanago1 <sr9plite@gmail.com>
2023-01-04 21:22:57 +0900

    追記

--- JS/main.js
+++ JS/main.js
@@ -5,6 +5,8 @@ const menuButton = document.querySelector(".L-header_mobileMenuContainer");
 const mobileMenu = document.querySelector(".mobileMenu");
 const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const menuClose = document.querySelector(".mobile-menu_close");
+
 
 // HTML elemental ===>
 
@@ -28,20 +30,22 @@ clear();
 // mobileMenu ==>
 const menuSwitch = () => {
     menuButton.addEventListener("click", () => {
-        mobileMenu.classList.toggle("mobileMenuOn");
-
-        if(mobileMenu.classList.contains("mobileMenuOn")) {
-            body.style.cssText = "overflow: hidden;" 
-            // container.style.cssText = "pointer-events: none;"
-        } else {
-            body.style.cssText = "overflow: scroll;"
-        //     container.style.cssText = "pointer-events: initial;"
-        }
+        mobileMenu.classList.add("mobileMenuOn");
+        body.classList.toggle("bodyNotScroll");
+        container.classList.add("menuOnContainer");
+        container.classList.add("bodyNotClick")
     })
 }
-// mobileMenu ==>
+
+const menuCloseSwitch = () => {
+    menuClose.addEventListener("click", () => {
+        mobileMenu.classList.toggle("mobileMenuOn");
+        body.classList.toggle("bodyNotScroll");
+        container.classList.remove("menuOnContainer");
+        container.classList.remove("bodyNotClick");
+    })
+}
 
 menuSwitch();
-
-
-
+menuCloseSwitch();
+// mobileMenu ==>
